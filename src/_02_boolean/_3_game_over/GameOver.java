c02f74cd5a771833aea7e9921fcf5cf944e9d818
league <richardson.liou@gmail.com>
2023-02-13 18:27:42 -0800

    i started module 4 today

--- src/_02_boolean/_3_game_over/GameOver.java
+++ src/_02_boolean/_3_game_over/GameOver.java
@@ -1,20 +1,25 @@
 package _02_boolean._3_game_over;
 
+import javax.management.openmbean.OpenDataException;
 import javax.swing.JOptionPane;
 
 public class GameOver {
 	
 	public static void main(String[] args) {
 
-		boolean gameOver = false;
+		
 		
 		// 1. Ask the user if the game is over and save their answer.
-		
-		// 2. If they answer "yes" change gameOver to true.
-		
+		String userResponse = JOptionPane.showInputDialog("is the game over?");
+		// 2. If they answer "yes" c {hange gameOver to true.
+		if(userResponse=="yes") {
+			boolean game0ver=true;
+		}
 		// 3. Surround steps 1-2 in a while loop that runs as long as
 		//    gameOver is false. Hint: Use the ! operator.
-		
+		while(game0ver=false) {
+			
+		}
 		// 4. Tell the user the game is over outside the while loop.
 		
 	}
